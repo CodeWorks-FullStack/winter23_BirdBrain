0cfe2d40676c82bf7cf981be7e2258b6111ff01c
Miles Collins <miles.collins@ymail.com>
2023-02-16 16:11:45 -0700

    Student Hand-off

--- client/app/Services/BirdsService.js
+++ client/app/Services/BirdsService.js
@@ -1,14 +1,27 @@
 import { appState } from "../AppState.js";
 import { Bird } from "../Models/Bird.js";
+import { Watcher } from "../Models/Watcher.js";
 import { server } from "./AxiosService.js";
 
 class BirdsService {
-  setActiveBird(birdId) {
+  async setActiveBird(birdId) {
     console.log("BIRDID", birdId);
     let bird = appState.birds.find((bird) => bird.id == birdId);
     console.log("SETTING ACTIVE BIRD", bird);
     appState.activeBird = bird;
+
+    // NOTE CREATED A NEW FUNCTION, THEN CALLED THE FUNCTION IN THE SETACTIVE
+    await this.getWatcherByBirdId(birdId);
   }
+
+  //
+  async getWatcherByBirdId(birdId) {
+    const res = await server.get(`api/birds/${birdId}/watchers`);
+    console.log("BIRD WATCHERS FOR THIS BIRD", res.data);
+    appState.watchers = res.data.map((watcher) => new Watcher(watcher));
+    // appState.emit("watchers");
+  }
+
   async reportBird(birdBody) {
     const res = await server.post("api/birds", birdBody);
     console.log("CREATING BIRD", res.data);
